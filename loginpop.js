5d15dd1325e078de89c3ee81f0e9876fc1c8628e
terryidiot <terryidiot8332@gmail.com>
2015-07-26 09:45:58 +0800

    0726_liang
    change
    -$(loginBox).fadeIn(500);

--- loginpop.js
+++ loginpop.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 		var loginBox = $(this).attr('href');
 
 		//Fade in the Popup and add close button
-		$(loginBox).fadeIn(300);
+		$(loginBox).fadeIn(500);
 		
 		//Set the center alignment padding + border
 		var popMargTop = ($(loginBox).height() + 24) / 2; 
